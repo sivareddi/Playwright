6956b2e54c6b40c2095c472aad202de7ffb5926c
sivareddi <ysivareddy@hotmail.com>
2026-07-23 14:33:05 +0530

    New files added

---
+++ 09_chapter_Loops/52_loop.js
@@ -0,0 +1,25 @@
+/*
+ICU 
+I - Initialization
+C - Condition
+U - Update
+*/
+//Print numbers from 0 to 9
+/*
+for (let i=0; i< 10; i++) {
+console.log(i);
+}
+*/
+console.log("FOR LOOP FROM HERE:");
+let i=0
+for (i = 0; i< 10; i++) {
+console.log(i);
+//i++
+}
+console.log("DO WHILE FROM HERE:");
+let a = 1;
+do {
+    console.log(a);
+    a++;
+}while(a<10)
+    console.log("value of a is: ", a);
