b3b88ebbf1d90273e7baef8238e474af8c43089b
sivareddi <ysivareddy@hotmail.com>
2026-07-27 19:33:18 +0530

    Add literal examples
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ 04_chapter_Literal/07_Literal.js
@@ -0,0 +1,20 @@
+let age = "Siva" // String literal
+console.log(age); // Siva
+let isStudent = true; // Boolean literal
+console.log(isStudent); // true
+let pi = 3.14; // Number literal    
+console.log(pi); // 3.14    
+let nullValue = null; // Null literal
+console.log(nullValue); // null
+let email; // Undefined literal
+console.log(email); // undefined
+let name = 'Alice'; // String literal
+console.log(name);
+
+console.log("Type of age: " + typeof age); // string
+console.log("Type of isStudent: " + typeof isStudent); // boolean
+console.log("Type of pi: " + typeof pi); // number
+console.log("Type of nullValue: " + typeof nullValue); // object
+console.log("Type of email: " + typeof email); // undefined, if variable is not assigned any value, it is considered undefined  
+ 
+
